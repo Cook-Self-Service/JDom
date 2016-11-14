fefcfed41c189c513d88ea1f8d8ced1f775b4a5c
Cook-Self-Service <support@j-cook.pro>
2016-11-14 22:00:17 +0100

    Removed legacy layer files

--- jdom.php
+++ jdom.php
@@ -4,38 +4,17 @@
  * @author		J. HUARD (http://j-cook.pro) - G. Tomaselli (http://bygiro.com)
  * @license     MIT License (MIT)
  */
- 
+
 defined('_JEXEC') or die;
 
 defined('DS') or define('DS',DIRECTORY_SEPARATOR);
 defined('BR') or define("BR", "<br />");
 defined('LN') or define("LN", "\n");
 
-if(!defined('PATH_LIBRARY_JDOM'))  define('PATH_LIBRARY_JDOM', JPATH_SITE . DS . 'libraries' . DS . 'jdom');
+if(!defined('PATH_LIBRARY_JDOM'))  define('PATH_LIBRARY_JDOM', JPATH_SITE . '/libraries/jdom');
 
 jimport('joomla.version');
 $version = new JVersion();
-
-if (!class_exists('CkJLoader'))
-{
-	// Joomla! 1.6 - 1.7
-	if (version_compare($version->RELEASE, '2.5', '<'))
-	{
-		// Load the missing class file
-		require_once(PATH_LIBRARY_JDOM .DS. 'legacy' .DS. 'loader.php');
-				
-		// Register the autoloader functions.
-		CkJLoader::setup();
-	}
-	
-	
-	//Joomla! 2.5 and later
-	else
-	{	
-		class CkJLoader extends JLoader{}
-	}
-}
-
 
 
 /**
@@ -43,7 +22,7 @@ if (!class_exists('CkJLoader'))
  *
  * @package     Joomla.plugin
  * @subpackage  System.jdom
- */ 
+ */
 class plgSystemJdom extends JPlugin
 {
     public function onAfterInitialise()
@@ -51,7 +30,7 @@ class plgSystemJdom extends JPlugin
 		// load plugin language file
 		$language = JFactory::getLanguage();
 		$language->load('plg_system_jdom', JPATH_ADMINISTRATOR);
-	
-		JLoader::register('JDom', JPATH_SITE . DS . 'libraries' . DS . 'jdom' . DS . 'dom.php');
+
+		JLoader::register('JDom', JPATH_SITE . '/libraries/jdom/dom.php');
     }
 }
